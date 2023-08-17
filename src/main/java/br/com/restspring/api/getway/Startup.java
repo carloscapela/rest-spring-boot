5ae16199e652789473972a7db2d4bf0f8b0ae4e5
Benedito Carlos <beneditocarlos.bc@gmail.com>
2023-08-16 21:26:13 -0300

    Iniciando estudos.

---
+++ src/main/java/br/com/restspring/api/getway/Startup.java
@@ -0,0 +1,12 @@
+package br.com.restspring.api.getway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+}
---
+++ src/test/java/br/com/restspring/api/getway/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.restspring.api.getway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
